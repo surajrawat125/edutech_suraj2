e564b756f10056bb3db0b9a3604582d626bf9cfc
surajrawat125 <dummy@gmail.com>
2026-04-12 17:42:00 +0000

    solve dummy initial commit

---
+++ server/src/main/java/com/edutech/progressive/EduConnectApplication.java
@@ -0,0 +1,10 @@
+package com.edutech.progressive;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduConnectApplication {
+    public static void main(String[] args) {
+        System.out.println("Welcome to EduConnect Progressive Project!");
+    }
+}
