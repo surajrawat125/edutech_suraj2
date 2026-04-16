735466453531b10632350f0f7a6cd1f2268476de
surajrawat125 <surajwork125@gmail.com>
2026-04-16 14:06:16 +0000

    day_11

--- server/src/main/java/com/edutech/progressive/dto/LoginRequest.java
+++ server/src/main/java/com/edutech/progressive/dto/LoginRequest.java
@@ -1,6 +1,29 @@
 package com.edutech.progressive.dto;
 
 public class LoginRequest {
-   private String username;
-   private String password;
+
+    // The username of the customer attempting to log in.
+    private String username;
+
+    // The password of the customer attempting to log in.
+    private String password;
+
+    public LoginRequest() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
